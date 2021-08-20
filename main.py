962b973b49e69b96b372c1f3c30a698e1ccfec26
Jeffrey Ricketts-Hagan <rickjeff44@gmail.com>
2021-08-19 22:58:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import requests
 
+#Sends request to Github
 response = requests.get("https://api.github.com/users/Jeffreyricketts/repos")
 my_repos = response.json()
+
+#Prints data in json
 print(my_repos)
+
+#Prints specific values from json code, this ccase: Repository Names and Repository Urls
 for repo in my_repos:
     print(f"Repository Name: {repo['name']} | Respository Url: {repo['html_url']}")
